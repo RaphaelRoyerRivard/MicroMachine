41063ab7f7c80f363625de59c520bf860e17f09e
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2018-09-30 20:56:35 -0400

    improved the resource gathering job assignation and removed the worker repair feature because it was making us lose against worker rush

--- src/WorkerManager.cpp
+++ src/WorkerManager.cpp
@@ -28,7 +28,7 @@ void WorkerManager::onFrame()
 
     m_workerData.drawDepotDebugInfo();
 
-    handleRepairWorkers();
+    //handleRepairWorkers();	// this has been commented out because it doesn't work well against worker rushes (less combat scvs and too much minerals are spent repairing)
 }
 
 void WorkerManager::setRepairWorker(Unit worker, const Unit & unitToRepair)
@@ -204,6 +204,9 @@ void WorkerManager::handleGasWorkers()
             // if it's less than we want it to be, fill 'er up
             for (int i=0; i<(3-numAssigned); ++i)
             {
+				if (getWorkerData().getWorkerJobCount(WorkerJobs::Minerals) <= getWorkerData().getWorkerJobCount(WorkerJobs::Gas))
+					break;
+
                 auto gasWorker = getGasWorker(unit);
                 if (gasWorker.isValid())
                 {
