ad9a62b36d8f822869a1ac92107ede205631881a
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2019-08-02 22:01:46 -0400

    batchfile

--- run_ladder_manager.bat
+++ run_ladder_manager.bat
@@ -1,2 +1,3 @@
 cd ~/Projects/Sc2LadderServer/build/bin/
+echo "MicroMachine"vs"MicroMachine" AutomatonLE.SC2Map > matchupList
 sudo ./Sc2LadderServer -e /home/raphael/StarCraftII/Versions/Base70154/SC2_x64
