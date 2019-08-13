5f794de433faf3caf6bd03b7109cd9300ca7319e
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2019-08-13 09:52:30 -0400

    added logs when starting a ladder game

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,19 @@
 #include <cstdio>
 #include <csignal>
 #include <cstdlib>
-#ifdef _WINDOWS
-#include "StackWalker.h"
-#endif
-
-#ifdef SC2API
 
 #include "sc2utils/sc2_manage_process.h"
 #include "sc2api/sc2_api.h"
 #include <string>
 #ifdef _WINDOWS
+	#include "StackWalker.h"
 	#include <direct.h>
 	#define getcwd _getcwd // stupid MSFT "deprecation" warning
 #else
+	#include <stdio.h>
+	#include <execinfo.h>
+	#include <signal.h>
+	#include <stdlib.h>
 	#include <unistd.h>
 #endif
 
@@ -49,7 +49,7 @@ std::string getexepath()
 	}
 	return s_cwd;
 #else
-	return "Not available on Linux";
+	return "Executable path not available on Linux";
 #endif
 }
 
@@ -227,101 +227,3 @@ int main(int argc, char* argv[])
     }
     return 0;
 }
-
-#else
-
-#include <BWAPI/Client.h>
-
-void UAlbertaBot_BWAPIReconnect() 
-{
-    while(!BWAPI::BWAPIClient.connect())
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds{ 1000 }); 
-    }
-}
-
-void UAlbertaBot_PlayGame()
-{
-    CCBot bot;
-    
-    // The main game loop, which continues while we are connected to BWAPI and in a game
-	while (BWAPI::BWAPIClient.isConnected() && BWAPI::Broodwar->isInGame()) 
-    {
-        // Handle each of the events that happened on this frame of the game
-		for (const BWAPI::Event & e : BWAPI::Broodwar->getEvents()) 
-        {
-			switch (e.getType()) 
-            {
-                case BWAPI::EventType::MatchStart:      { bot.OnGameStart();                 break; }
-			    case BWAPI::EventType::MatchFrame:      { bot.OnStep();                      break; }
-                                                        
-                /*case BWAPI::EventType::MatchEnd:        { m->onEnd(e.isWinner());            break; }
-			    case BWAPI::EventType::UnitShow:        { m->onUnitShow(e.getUnit());        break; }
-			    case BWAPI::EventType::UnitHide:        { m->onUnitHide(e.getUnit());        break; }
-			    case BWAPI::EventType::UnitCreate:      { m->onUnitCreate(e.getUnit());      break; }
-			    case BWAPI::EventType::UnitMorph:       { m->onUnitMorph(e.getUnit());       break; }
-			    case BWAPI::EventType::UnitDestroy:     { m->onUnitDestroy(e.getUnit());     break; }
-			    case BWAPI::EventType::UnitRenegade:    { m->onUnitRenegade(e.getUnit());    break; }
-			    case BWAPI::EventType::UnitComplete:    { m->onUnitComplete(e.getUnit());    break; }
-			    case BWAPI::EventType::SendText:        { m->onSendText(e.getText());        break; }*/
-			}
-		}
-
-		BWAPI::BWAPIClient.update();
-		if (!BWAPI::BWAPIClient.isConnected()) 
-        {
-			std::cout << "Disconnected\n";
-			break;
-		}
-	}
-
-    std::cout << "Game Over\n";
-}
-
-int main(int argc, char * argv[]) 
-{
-    bool exitIfStarcraftShutdown = true;
-
-    size_t gameCount = 0;
-	while (true)
-    {
-        // if we are not currently connected to BWAPI, try to reconnect
-        if (!BWAPI::BWAPIClient.isConnected())
-        {
-            UAlbertaBot_BWAPIReconnect();
-        }
-
-        // if we have connected to BWAPI
-        while (BWAPI::BWAPIClient.isConnected())
-        {
-            // wait for the game to start until the game starts
-            std::cout << "Waiting for game start\n";
-	        while (BWAPI::BWAPIClient.isConnected() && !BWAPI::Broodwar->isInGame()) 
-            {
-		        BWAPI::BWAPIClient.update();
-	        }
-
-            // Check to see if Starcraft shut down somehow
-            if (BWAPI::BroodwarPtr == nullptr)
-            {
-                break;
-            }
-
-            // If we are successfully in a game, call the module to play the game
-            if (BWAPI::Broodwar->isInGame())
-            {
-                std::cout << "Playing game " << gameCount++ << " on map " << BWAPI::Broodwar->mapFileName() << "\n";
-
-                UAlbertaBot_PlayGame();
-            }
-        }
-
-        if (exitIfStarcraftShutdown && !BWAPI::BWAPIClient.isConnected())
-        {
-            return 0;
-        }
-    }
-
-	return 0;
-}
-#endif
