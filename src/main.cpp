3c597b24dbb05d097a96f3aa6714daf3137162df
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2019-09-26 22:15:03 -0400

    fixed compilation error in demangling code

--- src/main.cpp
+++ src/main.cpp
@@ -72,13 +72,13 @@ void handler(int sig) {
 #else
 	// https://panthema.net/2008/0901-stacktrace-demangled/
 	// storage array for stack trace address data
-	void* addrlist[max_frames + 1];
+	void* addrlist[64];
 
 	// retrieve current stack addresses
 	int addrlen = backtrace(addrlist, sizeof(addrlist) / sizeof(void*));
 
 	if (addrlen == 0) {
-		fprintf(out, "  <empty, possibly corrupt>\n");
+		std::cout << "  <empty, possibly corrupt>" << std::endl;
 		return;
 	}
 	// resolve addresses into strings containing "filename(function+address)",
@@ -121,18 +121,18 @@ void handler(int sig) {
 				char* ret = abi::__cxa_demangle(begin_name, funcname, &funcnamesize, &status);
 				if (status == 0) {
 					funcname = ret; // use possibly realloc()-ed string
-					fprintf(out, "  %s : %s+%s\n", symbollist[i], funcname, begin_offset);
+					std::cout << "  " << symbollist[i] << " : " << funcname << "+" << begin_offset << std::endl;
 				}
 				else {
 					// demangling failed. Output function name as a C function with
 					// no arguments.
-					fprintf(out, "  %s : %s()+%s\n", symbollist[i], begin_name, begin_offset);
+					std::cout << "  " << symbollist[i] << " : " << begin_name << "()+" << begin_offset << std::endl;
 				}
 			}
 			else
 			{
 				// couldn't parse the line? print the whole line.
-				fprintf(out, "  %s\n", symbollist[i]);
+				std::cout << "  " << symbollist[i] << std::endl;
 			}
 		}
 
