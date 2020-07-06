27a89d1d02a60330fda1c9764cdac2095fbf439b
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2020-07-06 19:36:42 -0400

    commented debug create units

--- src/CCBot.cpp
+++ src/CCBot.cpp
@@ -1599,9 +1599,9 @@ void CCBot::IssueGameStartCheats()
 	Debug()->DebugCreateUnit(sc2::UNIT_TYPEID::PROTOSS_STALKER, mapCenter, player2, 1);*/
 
 	// Test to reproduce bug where Cyclone would not back after acquiring a Lock-On target
-	Debug()->DebugCreateUnit(sc2::UNIT_TYPEID::TERRAN_CYCLONE, m_startLocation - towardsCenter * 5, player2, 1);
+	/*Debug()->DebugCreateUnit(sc2::UNIT_TYPEID::TERRAN_CYCLONE, m_startLocation - towardsCenter * 5, player2, 1);
 	Debug()->DebugCreateUnit(sc2::UNIT_TYPEID::TERRAN_HELLION, m_startLocation + towardsCenter * 10, player1, 4);
-	Debug()->DebugCreateUnit(sc2::UNIT_TYPEID::TERRAN_REAPER, m_startLocation + towardsCenter * 10, player1, 4);
+	Debug()->DebugCreateUnit(sc2::UNIT_TYPEID::TERRAN_REAPER, m_startLocation + towardsCenter * 10, player1, 4);*/
 }
 
 void CCBot::IssueCheats()
