ba1c9d7a1765b6893e10f68dfd8a4dca4eaa8ff8
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2021-01-16 12:46:06 -0500

    will now wait for the armory to be finished before adding Thors to the production queue

--- src/ProductionManager.cpp
+++ src/ProductionManager.cpp
@@ -1064,8 +1064,8 @@ void ProductionManager::putImportantBuildOrderItemsInQueue()
 				const float cycloneTankRatio = float(cycloneCount) / std::max(1.f, float(tankCount));
 				const float enemySupplyAirGroundRatio = float(m_bot.Analyzer().opponentAirSupply) / std::max(1.f, float(m_bot.Analyzer().opponentGroundSupply));
 				const bool shouldProduceHellionsAgainstEarlyLightUnitsRush = factoryTechLabCount == 0 && earlyRushed && m_bot.Analyzer().getEnemyLightGroundUnitCount() > hellionCount;
-				// We want to build Thors against Protoss, but only after we have an Armory or 2 bases and we don't want more Thors than Cyclones
-				if (startingStrategy != PROXY_CYCLONES && enemyRace == sc2::Protoss && (finishedArmory || finishedBaseCount >= 2) && thorCount + 1 < cycloneCount)
+				// We want to build Thors against Protoss, but only after we have an Armory and we don't want more Thors than Cyclones
+				if (startingStrategy != PROXY_CYCLONES && enemyRace == sc2::Protoss && finishedArmory && thorCount + 1 < cycloneCount)
 				{
 					m_queue.removeAllOfType(MetaTypeEnum::Cyclone);
 					m_queue.removeAllOfType(MetaTypeEnum::SiegeTank);
