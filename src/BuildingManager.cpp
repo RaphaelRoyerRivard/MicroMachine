81981fd550de4e2ffb4d141a098fd52dc1ba1663
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2021-03-09 11:17:50 -0500

    deactivated the construction of CC in our main early on because it was causing bugs

--- src/BuildingManager.cpp
+++ src/BuildingManager.cpp
@@ -666,7 +666,7 @@ bool BuildingManager::assignWorkerToUnassignedBuilding(Building & b, bool filter
 		if (b.canBeBuiltElseWhere)
 		{
 
-			if (b.type.getAPIUnitType() == sc2::UNIT_TYPEID::TERRAN_COMMANDCENTER && m_bot.GetCurrentFrame() < 5448)//[expand in main first CC], before 4 minutes
+			if (b.type.getAPIUnitType() == sc2::UNIT_TYPEID::TERRAN_COMMANDCENTER && m_bot.GetCurrentFrame() < 0)//[expand in main first CC], before 4 minutes (temporarily set to 0 because it is causing bugs with long distance mining workers)
 			{
 				testLocation = m_bot.Buildings().getBuildingPlacer().getBuildLocationNear(b, false, true, false, true, true);
 			}
